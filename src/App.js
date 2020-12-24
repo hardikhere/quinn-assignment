9a11d17572a6ae3578046ad8573169cca22db150
hardikhere <web.hardikhere@gmail.com>
2020-12-24 22:20:21 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Calendar from './Components/Calendar';
+
+function App() {
+  return (
+    <div className="App">
+      <Calendar />
+    </div>
+  );
+}
+
+export default App;
