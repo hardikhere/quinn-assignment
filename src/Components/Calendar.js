3b822d146c0e6ba5407fc68b0134b9f5be805d9e
hardikhere <web.hardikhere@gmail.com>
2020-12-25 22:54:36 +0530

    dec calender

--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -1,20 +1,50 @@
 import React, { useEffect, useState } from 'react';
 import "./style.scss";
 
+const getDaysInMonth = (year, month) => {
+    return new Date(year, month + 1, 0).getDate()
+};
+const getStartDay = (year, month) => {
+    return new Date(year, month, 1).getDay();
+};
+const getEndDay = (year, month, nod) => {
+    return new Date(year, month, nod).getDay();
+};
+
 const Calendar = () => {
     const [weeks, setWeeks] = useState([
         "sun", "mon", "tue", "wed", "thu", "fri", "sat"
     ]);
     const [currentDate, setcurrentDate] = useState(new Date());
-    const [currentDatePage, setcurrentDatePage] = useState({
-        numberOfDays: new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate(),
-        startDay: new Date(currentDate.getFullYear(), currentDate.getMonth(), 1).getDay(),
-        endDay: new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.numberOfDays).getDay()
-    })
-
-    useEffect(()=>{
-        console.log(currentDatePage);
-    },[]);
+    const [currentDatePage, setcurrentDatePage] = useState();
+    const [Tiles, setTiles] = useState([])
+    useEffect(() => {
+        var today;
+        var year = new Date().getFullYear();
+        var month = new Date().getMonth();
+        var numberOfDays = getDaysInMonth(year, month);
+        var startDay = getStartDay(year, month);
+        var endDay = getEndDay(year, month, numberOfDays);
+        setcurrentDatePage({
+            numberOfDays,
+            startDay,
+            endDay
+        });
+        var tiles = [];
+        for (var i = 0; i < startDay; i++) {
+            tiles.push({
+                back: "b",
+                day: -1
+            });
+        };
+        for (var i = 0; i < numberOfDays; i++) {
+            tiles.push({
+                back: "o",
+                day: i
+            })
+        };
+        setTiles(tiles);
+    }, []);
 
     return (
         <div className="flex flex-jc-center flex-ai-center" style={{ height: "100vh" }}>
@@ -36,8 +66,12 @@ const Calendar = () => {
                 </div>
                 <div className="cal-days">
                     <div className="grid-container">
-                        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30].map(day => {
-                            return <div className="grid-item">{day}</div>
+                        {Tiles.map((day, ind) => {
+                            return <div className={`grid-item cal-days-${day.back}`}>
+                                {
+                                    day.back !== "b" ? day.day + 1 : null
+                                }
+                            </div>
                         })}
                     </div>
                 </div>
